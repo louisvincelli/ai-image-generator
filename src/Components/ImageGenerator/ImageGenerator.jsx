e5bdfa21a064c04d03455a4a5ea21494a42254e9
Louis Vincelli <louisvincelli@gmail.com>
2024-08-19 04:44:30 -0700

    final push

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from 'react';
+import './ImageGenerator.css';
+import default_image from '../Assets/default_image.jpg'
+
+export const ImageGenerator = () => {
+
+  const [image_url,setImage_url] = useState("/");
+  let inputRef = useRef(null);
+  const [loading,setLoading] = useState(false);
+
+  const imageGenerator = async () => {
+    if (inputRef.current.value==="") {
+        return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+        "https://api.openai.com/v1/images/generations",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:
+                "Bearer API_KEY",
+                "User-Agent":"Chrome",
+            },
+            body:JSON.stringify({
+                prompt:`${inputRef.current.value}`,
+                n:1,
+                size:"512x512",
+            })
+        }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
+  }
+
+  return (
+    <div className="ai-image-generator">
+        <div className="header">AI Image <span>Generator</span></div>
+        <div className="header2">By Louis Vincelli</div>
+        <div className="search-box">
+            <input type="text" ref={inputRef} className="search-input" placeholder="Describe your Image" />
+            <div className="generate-btn" onClick={()=>{imageGenerator()}}>Create AI Image</div>
+        </div>
+        <div className="img-loading">
+            <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
+            <div className="loading">
+                <div className={loading?"loading-bar-full":"loading-bar"}></div>
+                    <div className={loading?"loading-text":"display-none"}>Loading ...</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ImageGenerator
